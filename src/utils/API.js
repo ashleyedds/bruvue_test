3ae3d92dbfc336c7b35c62d783915ee412af2a3f
Ashley Edds <ashley@edds.net>
2018-07-03 20:20:05 -0400

    revert changes

--- src/utils/API.js
+++ src/utils/API.js
@@ -7,16 +7,6 @@ const beerURL = "https://s3.amazonaws.com/bruvue-data/beer-data.json";
 //Grab all the data from the API so we can use it in our application
 export default {
     searchBeer: () => {
-        fetch(beerURL, { mode: 'cors' })
-            .then(function (response) {
-                return response.text();
-            })
-            .then(function (text) {
-                console.log('Request successful', text);
-            })
-            .catch(function (error) {
-                console.log('Request failed', error)
-            });
+        return axios.get(beerURL)
     }
 }
-
